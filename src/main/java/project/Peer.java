acb26cf900545afab32a3169ae0f967c407df017
Andrew Whigham <awhigham9@gmail.com>
2021-10-20 16:59:37 -0400

    Add stub code to Peer, including message queue

--- src/main/java/project/Peer.java
+++ src/main/java/project/Peer.java
@@ -4,6 +4,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Peer {
 
@@ -26,6 +28,9 @@ public class Peer {
     // Peers of the current peer
     private ArrayList<PeerConfiguration> peers;
 
+    // Message Queue
+    private BlockingQueue<Message> messageQueue;
+
     public void Peer(String commonConfigPath, String peerConfigPath) throws FileNotFoundException, ParseException, IOException {
         CommonConfiguration commonConfig = new CommonConfiguration(commonConfigPath);
         commonConfig.load(); // Let it throw
@@ -36,5 +41,86 @@ public class Peer {
         this.filesize = commonConfig.filesize;
         this.piecesize = commonConfig.piecesize;
         this.peers = PeerConfiguration.loadPeerConfigurations(peerConfigPath); // Let it throw
+
+        // Linked List based queue
+        this.messageQueue = new LinkedBlockingQueue<>();
+    }
+
+    /**
+     * Attempts to push a message to the queue,
+     * and blocks (waits) if the queue is currently being
+     * accessed.
+     * @param msg - the Message to add
+     * @throws InterruptedException - possibly raised by putting into messageQueue
+     */
+    public void putMessage(Message msg) throws InterruptedException {
+        this.messageQueue.put(msg);
+    }
+
+    /**
+     * Launch the operation of the peer.
+     * Does not handle loading, but rather launches
+     * processes and continually handles messages
+     */
+    public void run() {
+        startUp();
+        while (true) {
+            try {
+                // Blocks until a message is available
+                Message msg = this.messageQueue.take();
+                handleMessage(msg);
+            }
+            catch (InterruptedException e) {
+                reportException(e);
+            }
+            catch (UnsupportedOperationException e) {
+                System.out.println("Hit not yet implemented code");
+                reportException(e);
+            }
+            catch (Exception e) {
+                // Unexpected exception
+                reportException(e);
+                System.out.println("Terminating due to unexpected exception");
+                break;
+            }
+        }
+        shutDown();
+    }
+
+    /**
+     * Start all functionality not launched at
+     * construction
+     */
+    public void startUp() {
+        // TODO -- Add startup functionality
+    }
+
+    /**
+     * Shutdown all active resources,
+     * such as threads, open files, etc.
+     */
+    public void shutDown() {
+        // TODO -- Add shutdown functionality
+    }
+
+    /**
+     * Handle a received message.
+     * Based on specification, may include a variety
+     * of actions based on message type.
+     * @param msg - message to be handled
+     */
+    private void handleMessage(Message msg) {
+        // TODO - Add message handling for each message type
+        throw new UnsupportedOperationException("handleMessage not yet implemented");
+    }
+
+    // TODO -- Make this dump to file and print
+    private static void reportException(Exception e) {
+        System.out.println("Exception caught");
+        String msg = e.getMessage();
+        if (msg != null) {
+            System.out.println(msg);
+        }
+        e.printStackTrace();
     }
 }
