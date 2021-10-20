6600f3c42168d8a90e29a9763129073a79ce6180
James Ulaszek <ulaszek@Jamess-MacBook-Pro.local>
2021-10-20 16:23:51 -0400

    Modified SampleClient, added Message and ClientTests

---
+++ src/test/java/project/ClientTests.java
@@ -0,0 +1,28 @@
+package project;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class ClientTests {
+
+    @Test
+    void callClient()
+    {
+
+    }
+
+    @Test
+    void receiveInput()
+    {
+
+    }
+
+    @Test
+    void sendMsg()
+    {
+
+    }
+}
