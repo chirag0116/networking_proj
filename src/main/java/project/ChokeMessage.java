91c9efbbacb1d69c30b8d074d5abb38d18ba6663
Andrew Whigham <awhigham9@gmail.com>
2021-10-21 13:36:12 -0400

    Added MessageFactory and Message-subclass ChokeMessage

---
+++ src/main/java/project/ChokeMessage.java
@@ -0,0 +1,47 @@
+package project;
+
+public class ChokeMessage extends Message {
+
+    private static final byte TYPE = 0;
+
+    /**
+     * Construct a ChokeMessage with a specified
+     * sender. Should be used when receiving messages.
+     * @param peer - The sender of the message (accessibly
+     *               by client).
+     */
+    public ChokeMessage(PeerConfiguration peer) {
+        this.peer = peer;
+    }
+
+    @Override
+    protected String getTypeBytes() {
+        byte[] bytes = {TYPE};
+        return new String(bytes);
+    }
+
+    @Override
+    protected String getPayloadBytes() {
+        return ""; // No payload
+    }
+
+    @Override
+    protected int getLength() {
+        return 5; // 4 length bytes + 1 type byte
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        else if (obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        final ChokeMessage other = (ChokeMessage) obj;
+        // Length and Type are equal since types are equal
+        return this.peer.equals(other.getPeer());
+
+    }
+}
---
+++ src/main/java/project/MessageFactory.java
@@ -0,0 +1,71 @@
+package project;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MessageFactory {
+
+    public Message makeMessage(String rawMsg, PeerConfiguration peer) {
+        // TODO - Verify this is the correct charset with tests
+        List<Byte> bytes = getByteList(rawMsg);
+
+        int length = lengthFromBytes(bytes.subList(0,4));
+        if (rawMsg.length() != length) {
+            throw new IllegalArgumentException("Length field does not match string received by MessageFactory");
+        }
+
+        byte type = bytes.get(4);
+        List<Byte> payload = Arrays.asList();
+        if (bytes.size() > 5) {
+            payload = bytes.subList(5,bytes.size() - 1);
+        }
+
+        Message msg = null;
+        switch(type) {
+            case 0:
+                msg = new ChokeMessage(peer);
+                break;
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+                /* TODO -- Add construction of each Message type
+                 * msg = MessageSubClass(params);
+                 * break;
+                 */
+            default:
+                throw new IllegalArgumentException("Unexpected message type in raw message");
+        }
+        return msg;
+    }
+
+    private List<Byte> getByteList(String s) {
+        byte[] byteArray = s.getBytes(StandardCharsets.US_ASCII);
+        List<Byte> bytes = new ArrayList<>();
+        for (byte b : byteArray) {
+            bytes.add(b);
+        }
+        return bytes;
+    }
+
+    /**
+     * Computes length (int) from list of 4 bytes
+     * @param lengthBytes - 4 byte list capturing bytes of the int
+     * @return length as an int
+     */
+    private int lengthFromBytes(List<Byte> lengthBytes) {
+        byte[] bytes = new byte[lengthBytes.size()];
+        for (int i = 0; i < lengthBytes.size(); i++) {
+            bytes[i] = lengthBytes.get(i);
+        }
+        ByteBuffer buf = ByteBuffer.wrap(bytes);
+        return buf.getInt();
+    }
+
+}
