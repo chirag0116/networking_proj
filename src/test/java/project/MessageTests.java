2fc1c1c0f5136841f22b592fa7aa010977819704
Andrew Whigham <awhigham9@gmail.com>
2021-10-21 13:57:33 -0400

    Add unequal case to testChokeMessageEquals

--- src/test/java/project/MessageTests.java
+++ src/test/java/project/MessageTests.java
@@ -5,23 +5,29 @@ import org.junit.jupiter.api.Test;
 
 public class MessageTests {
 
-    private static final PeerConfiguration PEER = new PeerConfiguration(1011, "lin114-00.cise.ufl.edu",6008,false);
+    private static final PeerConfiguration PEER1 = new PeerConfiguration(1011, "lin114-00.cise.ufl.edu",6008,false);
+    private static final PeerConfiguration PEER2 = new PeerConfiguration(1012, "lin114-01.cise.ufl.edu",6008,false);
+
+    private Message messageFromBytes(byte[] bytes, PeerConfiguration peer) {
+        String raw = new String(bytes);
+        MessageFactory factory = new MessageFactory();
+        return factory.makeMessage(raw, peer);
+    }
 
     @Test
     void testChokeMessageEquals() {
-        ChokeMessage m1 = new ChokeMessage(PEER);
-        ChokeMessage m2 = new ChokeMessage(PEER);
+        ChokeMessage m1 = new ChokeMessage(PEER1);
+        ChokeMessage m2 = new ChokeMessage(PEER1);
+        ChokeMessage m3 = new ChokeMessage(PEER2);
         Assertions.assertEquals(m1,m2);
+        Assertions.assertNotEquals(m1,m3);
     }
 
     @Test
     void testMessageFactoryChokeMessage() {
         byte[] bytes = {0,0,0,5,0};
-        String raw = new String(bytes);
-        MessageFactory factory = new MessageFactory();
-        Message received = factory.makeMessage(raw, PEER);
-        ChokeMessage expected = new ChokeMessage(PEER);
-
+        Message received = messageFromBytes(bytes, PEER1);
+        ChokeMessage expected = new ChokeMessage(PEER1);
         Assertions.assertTrue(received instanceof ChokeMessage);
         Assertions.assertEquals(expected, received);
     }
@@ -30,7 +36,7 @@ public class MessageTests {
     void testChokeMessageSerialization() {
         byte[] bytes = {0,0,0,5,0};
         String expected = new String(bytes);
-        ChokeMessage msg = new ChokeMessage(PEER);
+        ChokeMessage msg = new ChokeMessage(PEER1);
         String received = msg.serialize();
         Assertions.assertEquals(expected,received);
     }
@@ -40,14 +46,14 @@ public class MessageTests {
         byte[] bytes = {0,0,0,5,12};
         String raw = new String(bytes);
         MessageFactory factory = new MessageFactory();
-        Assertions.assertThrows(IllegalArgumentException.class, () -> factory.makeMessage(raw, PEER));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> factory.makeMessage(raw, PEER1));
     }
     @Test
     void testMessageFactoryInvalidLength() {
         byte[] bytes = {0,0,0,12,0};
         String raw = new String(bytes);
         MessageFactory factory = new MessageFactory();
-        Assertions.assertThrows(IllegalArgumentException.class, () -> factory.makeMessage(raw, PEER));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> factory.makeMessage(raw, PEER1));
     }
 
 }
