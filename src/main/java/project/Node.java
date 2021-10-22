1de48c160351604ec9735a8de713635e5d683591
James Ulaszek <ulaszek@Jamess-MacBook-Pro.local>
2021-10-21 23:34:13 -0400

    retouch for unneccesary imports

--- src/main/java/project/Node.java
+++ src/main/java/project/Node.java
@@ -6,10 +6,8 @@ import java.net.Socket;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
-import java.util.ArrayList;
 import java.util.Hashtable;
-import java.util.List;
-import java.util.Set;
+
 
 public class Node {
     private int maxPeers;
