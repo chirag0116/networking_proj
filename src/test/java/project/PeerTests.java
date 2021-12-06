2893c78c8a0fbadc312da7906cc36a1e0d4e9052
Andrew Whigham <awhigham9@gmail.com>
2021-12-05 22:01:23 -0500

    Add test for computePreferredNeighbors

--- src/test/java/project/PeerTests.java
+++ src/test/java/project/PeerTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class PeerTests {
 
@@ -28,5 +30,43 @@ public class PeerTests {
         interested.put(4, true);
 
         Assertions.assertEquals(Peer.pickOptUnchokedNeighbor(peers, preferred, interested), 2);
+    }
+
+    @Test
+    void testComputePreferredNeighbors() {
+        ArrayList<PeerConfiguration> peers = new ArrayList<>(Arrays.asList(
+            new PeerConfiguration(1,"foo",8000,false),
+            new PeerConfiguration(2,"foo",8000,false),
+            new PeerConfiguration(3,"foo",8000,false),
+            new PeerConfiguration(4,"foo",8000,false)
+        ));
+        ConcurrentMap<Integer, Integer> scores = new ConcurrentHashMap<>();
+        scores.put(1,10);
+        scores.put(2,5);
+        scores.put(3,0);
+        scores.put(4,5);
+
+        ConcurrentMap<Integer,Boolean> result = Peer.computePreferredNeighbors(peers, scores, 1);
+        Assertions.assertTrue(result.get(1));
+        Assertions.assertFalse(result.get(2));
+        Assertions.assertFalse(result.get(3));
+        Assertions.assertFalse(result.get(4));
+
+        result = Peer.computePreferredNeighbors(peers, scores, 2);
+        Assertions.assertTrue(result.get(1));
+        Assertions.assertTrue(result.get(2) ^ result.get(4));
+        Assertions.assertFalse(result.get(3));
+
+        result = Peer.computePreferredNeighbors(peers, scores, 3);
+        Assertions.assertTrue(result.get(1));
+        Assertions.assertTrue(result.get(2));
+        Assertions.assertFalse(result.get(3));
+        Assertions.assertTrue(result.get(4));
+
+        result = Peer.computePreferredNeighbors(peers, scores, 4);
+        Assertions.assertTrue(result.get(1));
+        Assertions.assertTrue(result.get(2));
+        Assertions.assertTrue(result.get(3));
+        Assertions.assertTrue(result.get(4));
     }
 }
