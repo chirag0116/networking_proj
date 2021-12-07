0170e5e0f594d762826a3e5ff2105cb4489e4ef5
Drew <abrown5@ufl.edu>
2021-12-06 23:22:58 -0500

    added logs

--- src/main/java/project/Peer.java
+++ src/main/java/project/Peer.java
@@ -328,6 +328,7 @@ public class Peer {
         if (msg instanceof ChokeMessage) {
             ChokeMessage m = (ChokeMessage) msg;
             response = handleChokeMessage(m);
+            mLog.logChoked(self.getId() ,msg.getPeer().getId());
         }
         else if (msg instanceof UnchokeMessage) {
             UnchokeMessage m = (UnchokeMessage) msg;
@@ -337,6 +338,7 @@ public class Peer {
         else if (msg instanceof InterestedMessage) {
             if (!interested.get(msg.getPeer().getId())) {
                 interested.put(msg.getPeer().getId(), true);
+                mLog.logInterested(self.getId() ,msg.getPeer().getId());
             }
             else {
                 System.out.println("Peer is already interested");
@@ -345,6 +347,7 @@ public class Peer {
         else if (msg instanceof UninterestedMessage) {
             if (interested.get(msg.getPeer().getId())) {
                 interested.put(msg.getPeer().getId(), false);
+                mLog.logNotInterested(self.getId() ,msg.getPeer().getId());
             }
             else {
                 System.out.println("Peer is already not interested");
